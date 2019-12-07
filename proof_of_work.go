f7afcf6b45f03dbd894b0ec99d6ecb72cd567de8
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-12-07 23:07:13 +0900

    feat: impl prerare data

--- proof_of_work.go
+++ proof_of_work.go
@@ -1,12 +1,32 @@
 package main
 
-import "math/big"
+import (
+	"bytes"
+	"math/big"
+)
 
 const targetBits = 24
 
 type ProofOfWork struct {
 	block  *Block
 	target *big.Int
+}
+
+func (pow *ProofOfWork) prepareData(nouce int) []byte {
+
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PrevBlockHash,
+			pow.block.Data,
+			IntToHex(pow.block.Timestamp),
+			IntToHex(int64(targetBits)),
+			IntToHex(int64(nouce)),
+		},
+		[]byte{},
+	)
+
+	return data
+
 }
 
 func NewProofOfWork(b *Block) *ProofOfWork {
